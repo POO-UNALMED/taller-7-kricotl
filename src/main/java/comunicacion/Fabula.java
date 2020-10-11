2e28c01de7db6769986a721a41eaa04aed344d80
Saherrerap <69688276+Saherrerap@users.noreply.github.com>
2020-10-11 13:41:21 -0500

    Solucion 1.3

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -3,9 +3,9 @@ package comunicacion;
 public class Fabula extends Escrito {
 	private String ensenanza;
 	private String interpretacion;
-	public Fabula(String origen, String titulo, String autor, int paginas, String enseñanza, String interpretacion) {
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
 		super(origen, titulo, autor, paginas);
-		this.ensenanza = enseñanza;
+		this.ensenanza = ensenanza;
 		this.interpretacion = interpretacion;
 	}
 
